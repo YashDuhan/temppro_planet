c0e00b6308f6c568b8b0afe7ddd2ff46dd67c8f0
Yash Duhan <70028651+YashDuhan@users.noreply.github.com>
2024-03-08 23:02:39 +0530

    2

---
+++ main/js/main.js
@@ -0,0 +1,9 @@
+/*PreLoader*/ 
+var loader = document.getElementById("preloader");
+
+window.addEventListener("load", function() {
+    this.setTimeout(function()
+    {
+        loader.style.display = "none";
+    },2000); //Add a 2000 milliseconds(2 Sec)
+})
