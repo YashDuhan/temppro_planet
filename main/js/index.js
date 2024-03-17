d8b08333c371c573556c28ca08c71c5c453ccc79
Yash Duhan <70028651+YashDuhan@users.noreply.github.com>
2024-03-17 11:48:29 +0530

    fixed video delay

--- main/js/index.js
+++ main/js/index.js
@@ -1,3 +1,4 @@
+//Preloader
 document.addEventListener("DOMContentLoaded", function() {
     var counter = document.getElementById("counter");
     var percent = 0;
@@ -10,26 +11,4 @@ document.addEventListener("DOMContentLoaded", function() {
             document.getElementById("loader").style.display = "none";
         }
     }, 20); // Adjust the interval as needed
-
-    var video = document.getElementById("bg-video");
-
-    video.addEventListener("progress", function() {
-        // Calculate video loading progress
-        var progress = 0;
-        if (video.duration > 0) {
-            for (var i = 0; i < video.buffered.length; i++) {
-                progress += video.buffered.end(i);
-            }
-            progress = Math.floor((progress / video.duration) * 100);
-        }
-
-        // Update the loader progress to sync with video loading
-        counter.textContent = progress + "%";
-        if (progress >= 100) {
-            // Once the video is fully loaded, hide the loader
-            document.getElementById("loader").style.display = "none";
-            // Show the video
-            video.style.display = "block";
-        }
-    });
 });
